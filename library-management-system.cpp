a404826c0454ff9d179acbd7194d5813a22fe9a5
Aziz Ur Rehman <eazizurrehman@gmail.com>
2024-06-10 05:46:57 +0500

    finish project

--- library-management-system.cpp
+++ library-management-system.cpp
@@ -35,7 +35,7 @@ public:
         cin >> student.contactNo;
 
         ofstream file;
-        file.open("./students/all-students-list.txt", ios::app);
+        file.open("./database/students/all-students-list.txt", ios::app);
 
         file << student.ARID_No << " " << student.name << " " << student.fatherName << " " << student.contactNo << endl;
         file.close();
@@ -47,8 +47,8 @@ public:
         ifstream inFile;
         ofstream outFile;
 
-        inFile.open("./students/all-students-list.txt");
-        outFile.open("./students/temp.txt");
+        inFile.open("./database/students/all-students-list.txt");
+        outFile.open("./database/students/temp.txt");
 
         // if system fails to open the original file
         if (!inFile)
@@ -59,7 +59,7 @@ public:
 
         if (!outFile)
         {
-            cout << "\nSystem is getting internal error in deleting student. Try again later." << endl;
+            cout << "\nInternal system error while deleting student. Try again later." << endl;
             return;
         }
 
@@ -85,15 +85,15 @@ public:
         inFile.close();
         outFile.close();
 
-        remove("./students/all-students-list.txt");
-        rename("./students/temp.txt", "./students/all-students-list.txt");
+        remove("./database/students/all-students-list.txt");
+        rename("./database/students/temp.txt", "./database/students/all-students-list.txt");
 
         cout << "\nStudent with ARID No - " << ARID_No << " deleted successfully!!" << endl;
     }
 
     void deleteAllStudents()
     {
-        ifstream file("./students/all-students-list.txt");
+        ifstream file("./database/students/all-students-list.txt");
 
         // TODO: if file exists but has no data
         if (!file)
@@ -103,7 +103,7 @@ public:
         }
 
         file.close();
-        remove("./students/all-students-list.txt");
+        remove("./database/students/all-students-list.txt");
 
         cout << "\nAll students deleted successfully!!" << endl;
     }
@@ -111,7 +111,7 @@ public:
     void displayAllStudents()
     {
         ifstream file;
-        file.open("./students/all-students-list.txt");
+        file.open("./database/students/all-students-list.txt");
 
         if (!file)
         {
@@ -126,7 +126,7 @@ public:
 
         while (file >> student.ARID_No >> student.name >> student.fatherName >> student.contactNo)
         {
-            cout << "\t" << student.ARID_No << "\t\t" << student.name << "\t\t" << student.fatherName << "\t\t" << student.contactNo << endl;
+            cout << "\t" << student.ARID_No << "\t\t" << student.name << "\t\t" << student.fatherName << "\t\t\t" << student.contactNo << endl;
         }
 
         file.close();
@@ -165,7 +165,7 @@ public:
         cin >> book.publisher;
 
         ofstream file;
-        file.open("./books/all-books-list.txt", ios::app);
+        file.open("./database/books/all-books-list.txt", ios::app);
 
         file << book.ISBN << " " << book.title << " " << book.author << " " << book.publisher << endl;
         file.close();
@@ -177,8 +177,8 @@ public:
         ifstream inFile;
         ofstream outFile;
 
-        inFile.open("./books/all-books-list.txt");
-        outFile.open("./books/temp.txt");
+        inFile.open("./database/books/all-books-list.txt");
+        outFile.open("./database/books/temp.txt");
 
         // if system fails to open the original file
         if (!inFile)
@@ -189,7 +189,7 @@ public:
 
         if (!outFile)
         {
-            cout << "\nSystem is getting internal error in deleting book. Try again later." << endl;
+            cout << "\nInternal system error while deleting book. Try again later." << endl;
             return;
         }
 
@@ -215,15 +215,15 @@ public:
         inFile.close();
         outFile.close();
 
-        remove("./books/all-books-list.txt");
-        rename("./books/temp.txt", "./books/all-books-list.txt");
+        remove("./database/books/all-books-list.txt");
+        rename("./database/books/temp.txt", "./database/books/all-books-list.txt");
 
         cout << "\nBook with ISBN - " << ISBN << " deleted successfully!!" << endl;
     }
 
     void deleteAllBooks()
     {
-        ifstream file("./books/all-books-list.txt");
+        ifstream file("./database/books/all-books-list.txt");
 
         // TODO: if file exists but has no data
         if (!file)
@@ -233,7 +233,7 @@ public:
         }
 
         file.close();
-        remove("./books/all-books-list.txt");
+        remove("./database/books/all-books-list.txt");
 
         cout << "\nAll books deleted successfully!!" << endl;
     }
@@ -241,7 +241,7 @@ public:
     void displayAllBooks()
     {
         ifstream file;
-        file.open("./books/all-books-list.txt");
+        file.open("./database/books/all-books-list.txt");
 
         if (!file)
         {
@@ -265,12 +265,197 @@ public:
 
 int Books::count = 1000;
 
+class Library
+{
+public:
+    void allotBook()
+    {
+        // opening all the files
+        ifstream studentsFile("./database/students/all-students-list.txt");
+        ifstream booksFile("./database/books/all-books-list.txt");
+        ifstream libraryFile("./database/library/all-library-list.txt");
+
+        if (!studentsFile || !libraryFile || !booksFile)
+        {
+            cout << "Internal system error while allotting the book to student. Try again later." << endl;
+            return;
+        }
+
+        int ARID_No;
+        cout << "\tEnter student ARID No: ";
+        cin >> ARID_No;
+
+        // check if the student exists in students file
+        Student student;
+        bool studentExists = false;
+
+        while (studentsFile >> student.ARID_No >> student.name >> student.fatherName >> student.contactNo)
+        {
+            if (student.ARID_No == ARID_No)
+            {
+                studentExists = true;
+                break;
+            }
+        }
+
+        studentsFile.close();
+
+        if (!studentExists)
+        {
+            cout << "No Student exist with the provided ARID No." << endl;
+            return;
+        }
+
+        // check if the already 3 books are alloted
+        int allottedARID, allottedISBN, bookCount = 0;
+
+        while (libraryFile >> allottedARID >> allottedISBN)
+        {
+            if (allottedARID == ARID_No)
+            {
+                bookCount++;
+            }
+        }
+
+        libraryFile.close();
+
+        if (bookCount >= 3)
+        {
+            cout << "Student has already been allotted 3 books." << endl;
+            return;
+        }
+
+        // check if the book exists in books file
+        int ISBN;
+        cout << "\tEnter book ISBN: ";
+        cin >> ISBN;
+
+        Book book;
+        bool bookExists = false;
+
+        while (booksFile >> book.ISBN >> book.title >> book.author >> book.publisher)
+        {
+            if (book.ISBN == ISBN)
+            {
+                bookExists = true;
+                break;
+            }
+        }
+
+        booksFile.close();
+
+        if (!bookExists)
+        {
+            cout << "No book exist with the provided ISBN." << endl;
+            return;
+        }
+
+        // check if the requested book is already alloted
+
+        libraryFile.open("./database/library/all-library-list.txt");
+
+        bool isBookAlreadyAlloted = false;
+
+        while (libraryFile >> allottedARID >> allottedISBN)
+        {
+            if (allottedARID == ARID_No && allottedISBN == ISBN)
+            {
+                isBookAlreadyAlloted = true;
+                break;
+            }
+        }
+
+        libraryFile.close();
+
+        if (isBookAlreadyAlloted)
+        {
+            cout << "The requested book is already alloted. Can't allot a book twice to a student" << endl;
+            return;
+        }
+
+        // save the newly alloted book record in the file
+        ofstream file;
+        file.open("./database/library/all-library-list.txt", ios::app);
+
+        if (!file)
+        {
+            cout << "Internal system error while allotting the book to student. Try again later." << endl;
+            return;
+        }
+
+        file << ARID_No << " " << ISBN << endl;
+
+        file.close();
+
+        cout << "\nBook alloted to the student successfully." << endl;
+    }
+
+    void returnBook()
+    {
+        ifstream libraryFile("./database/library/all-library-list.txt");
+
+        if (!libraryFile)
+        {
+            cout << "Internal system error while returning the book. Try again later." << endl;
+            return;
+        }
+
+        int ARID_No;
+        cout << "\tEnter student ARID No: ";
+        cin >> ARID_No;
+
+        int ISBN;
+        cout << "\tEnter book ISBN: ";
+        cin >> ISBN;
+
+        ofstream tempFile("./database/library/temp.txt");
+
+        if (!tempFile)
+        {
+            cout << "Internal system error while returning the book. Try again later." << endl;
+            return;
+        }
+
+        int allottedARID, allottedISBN;
+        bool recordFound = false;
+
+        while (libraryFile >> allottedARID >> allottedISBN)
+        {
+            if (allottedARID == ARID_No && allottedISBN == ISBN)
+            {
+                recordFound = true;
+            }
+            else
+            {
+                tempFile << allottedARID << " " << allottedISBN << endl;
+            }
+        }
+
+        libraryFile.close();
+        tempFile.close();
+
+        if (!recordFound)
+        {
+            cout << "No record found for the provided ARID No and ISBN." << endl;
+            remove("./database/library/temp.txt");
+            return;
+        }
+
+        remove("./database/library/all-library-list.txt");
+        rename("./database/library/temp.txt", "./database/library/all-library-list.txt");
+
+        cout << "\nBook returned successfully." << endl;
+    }
+};
+
 int main()
 {
-    cout << "\n** Welcome to the Library Management System **" << endl;
+    cout << "\n***** Welcome to the Library Management System *****" << endl;
 
     Students students;
     Books books;
+    Library library;
+
     int choice;
 
     do
@@ -286,7 +471,6 @@ int main()
              << "\t8.\tDisplay all books" << endl
              << "\t9.\tAllot a Book" << endl
              << "\t10.\tReturn a Book" << endl
-             << "\t11.\tCheck alloted books to a student" << endl
              << "\t99.\tExit" << endl
              << "\nEnter your choice: ";
 
@@ -319,16 +503,13 @@ int main()
             books.displayAllBooks();
             break;
         case 9:
-            // Add your code for allotting a book here
+            library.allotBook();
             break;
         case 10:
-            // Add your code for returning a book here
-            break;
-        case 11:
-            // Add your code for checking alloted books to a student here
+            library.returnBook();
             break;
         case 99:
-            cout << "\n** Thanks for using the Library Management System **" << endl;
+            cout << "\n***** Thanks for using the Library Management System *****" << endl;
             break;
         default:
             cout << "Invalid choice , Enter again ";
